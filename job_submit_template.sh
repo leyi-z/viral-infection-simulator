1cef01b5c41f775691858e9d99aa145f966a88d8
Leyi Zhang <leyizhang2020@gmail.com>
2024-06-23 13:53:22 -0400

    finalized before running simulations - with more realistic time and memory requests

--- job_submit_template.sh
+++ job_submit_template.sh
@@ -3,8 +3,8 @@
 #SBATCH -p volta-gpu
 #SBATCH -N 1
 #SBATCH -n 1
-#SBATCH --mem=1G
-#SBATCH -t 00:04:00
+#SBATCH --mem=6G
+#SBATCH -t 00:20:00
 #SBATCH --qos gpu_access
 #SBATCH --gres=gpu:1
 #SBATCH --output=results/output/slurm-%x-%j.out
